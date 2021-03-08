e9e7be8159155b637457cf8d2c95886384b70680
WMY <mingyuewu@caih.com>
2021-03-08 09:13:07 +0800

    上传代码

---
+++ app/src/main/java/com/caih/jinge/Kinggrid.java
@@ -0,0 +1,12 @@
+package com.caih.jinge;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Kinggrid {
+
+    public static void init(Context context, String token){
+        Toast.makeText(context, "初始化成功 token："+token, Toast.LENGTH_LONG).show();
+    }
+
+}
